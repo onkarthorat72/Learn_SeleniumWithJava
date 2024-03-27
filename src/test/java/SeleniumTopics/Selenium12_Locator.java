f0ff2c0ae1faa9ee0faad9b04c6f6b44f402a262
onkarthorat72 <onkarthorat95@gmail.com>
2024-03-27 21:31:03 +0530

    Understand Each code line and write as in your language...

--- src/test/java/SeleniumTopics/Selenium12_Locator.java
+++ src/test/java/SeleniumTopics/Selenium12_Locator.java
@@ -8,8 +8,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+/**
+ * This class demonstrates the usage of locators in Selenium WebDriver
+ * to find web elements and perform actions on them.
+ */
 public class Selenium12_Locator {
 
+    /**
+     * This test case finds elements and attempts to log in with invalid credentials.
+     * It then verifies the error message displayed on the page.
+     */
     @Test(groups = "QA")
     @Description("Find elements and login with invalid credentials")
     public static void main(String[] args) {
@@ -21,20 +29,22 @@ public class Selenium12_Locator {
         driver.findElement(By.id("js-login-btn")).click();
 
         try {
+            // Wait for 3 seconds (not recommended in production code)
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
+        // Find the error message element
         WebElement error_msg = driver.findElement(By.id("js-notification-box-msg"));
-        String error_msg_text=error_msg.getText();
-        Assert.assertEquals(error_msg_text,"Your email, password, IP address or location did not match");
+
+        // Get the text of the error message element
+        String error_msg_text = error_msg.getText();
+
+        // Assert that the error message text matches the expected value
+        Assert.assertEquals(error_msg_text, "Your email, password, IP address or location did not match");
+
+        // Close the browser
         driver.quit();
-
-
-
     }
-
 }
-
-
--- src/test/java/SeleniumTopics/Selenium17TestNGFlow.java
+++ src/test/java/SeleniumTopics/Selenium17TestNGFlow.java
@@ -1,25 +1,40 @@
+// Package declaration
 package SeleniumTopics;
 
+// Import required classes and libraries
 import io.qameta.allure.Description;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+/**
+ * This class demonstrates the basic flow of TestNG and the usage of annotations
+ * for test setup, test execution, and test teardown.
+ */
 public class Selenium17TestNGFlow {
 
-    // Atomic Test Cases
-    // TC who don't have any dep.
-    // They serve single purpose 0
-
+    // Declare an instance variable to store the EdgeDriver instance
     EdgeDriver driver;
 
+    /**
+     * This method is executed before running any test cases in the class.
+     * It initializes the EdgeDriver instance and creates a new browser session.
+     */
     @BeforeTest
     public void openBrowser() {
         // Create Session via the API and Session ID is generated
         driver = new EdgeDriver();
     }
 
+    /**
+     * This is a test case that navigates to the CURA Healthcare Service website.
+     * It performs the following steps:
+     * 1. Navigate to the "https://katalon-demo-cura.herokuapp.com/" website.
+     * 2. Maximize the browser window.
+     *
+     * @throws InterruptedException if the thread is interrupted while waiting
+     */
     @Test(groups = "QA")
     @Description("Test Case Description")
     public void testVWOLogin_positive() throws InterruptedException {
@@ -27,11 +42,12 @@ public class Selenium17TestNGFlow {
         driver.manage().window().maximize();
     }
 
-
+    /**
+     * This method is executed after all test cases in the class have finished running.
+     * It calls the quit() method on the driver instance to close the browser and terminate the WebDriver session.
+     */
     @AfterTest
     public void closeBrowser() {
         driver.quit();
-
-
     }
 }
