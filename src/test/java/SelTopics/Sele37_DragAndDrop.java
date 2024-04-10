72c72152d6ae0755f6574f05e421de85010566df
onkarthorat72 <onkarthorat95@gmail.com>
2024-04-10 18:18:55 +0530

    Selenium Java Tutorials

---
+++ src/test/java/SelTopics/Sele37_DragAndDrop.java
@@ -0,0 +1,40 @@
+package SelTopics;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Sele37_DragAndDrop {
+
+    EdgeDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+        EdgeOptions options = new EdgeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--guest");
+        driver = new EdgeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @Test(groups = "QA")
+    @Description("Test Case Description")
+    public void testPositive() throws InterruptedException {
+        String URL = "https://www.makemytrip.com/";
+        driver.get(URL);
+        Actions actions = new Actions(driver);
+
+
+    }
+
+//    @AfterTest
+//    public void closeBrowser() {
+//        driver.quit();
+//    }
+
+}
