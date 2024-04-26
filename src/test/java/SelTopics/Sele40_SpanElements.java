806cc01ec6eae2f41e4a1a647d22642d7005a204
onkarthorat72 <onkarthorat95@gmail.com>
2024-04-26 16:16:16 +0530

    Selenium Java Tutorials -Practice

---
+++ src/test/java/SelTopics/Sele40_SpanElements.java
@@ -0,0 +1,42 @@
+package SelTopics;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class Sele40_SpanElements {
+
+    EdgeDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+        EdgeOptions options = new EdgeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--guest");
+        driver = new EdgeDriver(options);
+    }
+
+    @Test(groups = "QA")
+    @Description("Test Case Description")
+    public void testPositive() throws InterruptedException {
+        driver.get("https://awesomeqa.com/practice.html");
+        driver.manage().window().maximize();
+        WebElement span_element = driver.findElement(By.xpath("//span[normalize-space()='Years of Experience']"));
+        driver.findElement(with(By.id("exp-3")).toRightOf(span_element)).click();
+
+        Thread.sleep(5000);
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/SelTopics/Sele41_ListOfStates.java
@@ -0,0 +1,64 @@
+package SelTopics;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class Sele41_ListOfStates {
+
+    EdgeDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+        EdgeOptions options = new EdgeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--guest");
+        driver = new EdgeDriver(options);
+    }
+
+
+    @Test(groups = "QA")
+    @Description("Test Case Description")
+    public void testPositive() throws InterruptedException {
+        driver.get("https://www.aqi.in/real-time-most-polluted-city-ranking");
+        driver.manage().window().maximize();
+
+        Thread.sleep(5000);
+
+        WebElement searchelement = driver.findElement(By.xpath("//input[@id=\"search_city\"]"));
+        searchelement.sendKeys("India" + Keys.ENTER);
+
+        ////table[@id='example']/tbody/tr/td[2]
+
+        List<WebElement> list_of_states = driver.findElements(By.xpath("//table[@id='example']/tbody/tr/td[2]"));
+
+
+        for (WebElement l : list_of_states) {
+            String s1 = driver.findElement(with(By.tagName("p")).toRightOf(l)).getText();
+            String s2 = driver.findElement(with(By.tagName("p")).toLeftOf(l)).getText();
+            String s3 = driver.findElement(with(By.tagName("p")).below(l)).getText();
+            String s4 = driver.findElement(with(By.tagName("p")).above(l)).getText();
+            System.out.println(l.getText() + " | " + s1 + " | " + s2);
+            System.out.println(l.getText() + " | " + s3 + " | " + s4);
+        }
+
+        Thread.sleep(5000);
+
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
